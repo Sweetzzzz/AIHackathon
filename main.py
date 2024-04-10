c309bb8aa4b9582362106ecf5c0213bc90b610f6
Sam Ly <sam@samly.me>
2024-04-10 00:03:01 -0700

    style: move code into main function

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from ollama import Client
-client = Client(host='http://localhost:11434')
-for part in client.chat(model='syssum', stream=True, messages=[
-  {
-    'role': 'user',
-    'content': 'what is photosynthesis?',
-  },
-]):
-    print(part['message']['content'], end='', flush=True)
+
+def main():
+    client = Client(host='http://localhost:11434')
+    for part in client.chat(model='syssum', stream=True, messages=[
+    {
+        'role': 'user',
+        'content': 'what is photosynthesis?',
+    },
+    ]):
+        print(part['message']['content'], end='', flush=True)
+
+if __name__ == '__main__':
+    main()
 # Goal:
 #  - split the information in the notes into individual messages
 #  - store messages somewhere, I will make a mock data for now
