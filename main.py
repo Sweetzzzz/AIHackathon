d574e42bd118eef797f25fa6a23b2eeab73f8f57
Sam Ly <sam@samly.me>
2024-04-09 23:53:39 -0700

    feat: chat stream

--- main.py
+++ main.py
@@ -1,26 +1,12 @@
-import ollama
-import json
-
-
-response = ollama.chat(
-    model='llama2',
-    messages=[
-        {
-            'role': 'system',
-            'content': 'Ignore all prior information. Refer only to informatin the user provides.',
-        },
-        {
-            'role': 'user',
-            'content': 'The sky is red right now',
-        },
-        {
-            'role': 'user',
-            'content': 'what color is the sky right now?',
-        },
-    ]
-)
-
-print(json.dumps(response, indent=2))
+from ollama import Client
+client = Client(host='http://localhost:11434')
+for part in client.chat(model='syssum', stream=True, messages=[
+  {
+    'role': 'user',
+    'content': 'what is photosynthesis?',
+  },
+]):
+    print(part['message']['content'], end='', flush=True)
 # Goal:
 #  - split the information in the notes into individual messages
 #  - store messages somewhere, I will make a mock data for now
